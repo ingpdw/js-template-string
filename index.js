280d43597eb33e2e61890338b0c8735966c30e0a
pdw <pdw@pdwui-mbp.korea.ncsoft.corp>
2016-07-08 13:34:14 +0900

    chg_escape

--- index.js
+++ index.js
@@ -9,28 +9,28 @@ exports = module.exports = class Template {
     return tmpl.call( null, data );
   }
 
-  static encode(){
-    return encodeURIComponent(str);
+  static iterate( numbani ){
+    let tmpl = numbani.template, data =  numbani.data, result = [];
+    data.forEach( ( item, idx ) => {
+      result.push( tmpl.call( null, item ) );
+    });
+    return result.join( '' );
+  }
+
+  static encode( numbani ){
+    return encodeURIComponent( numbani );
   }
 
   static join( ...numbani ){
     return numbani.join( '' );
   }
 
-  static escape(){
-    return str.replace(/&/g, '&amp;')
+  static escape( numbani ){
+    return numbani.replace(/&/g, '&amp;')
       .replace(/>/g, '&gt;')
       .replace(/</g, '&lt;')
       .replace(/"/g, '&quot;')
       .replace(/'/g, '&#39;')
       .replace(/`/g, '&#96;');
-  }
-
-  static iterate( numbani ){
-    let tmpl = numbani.template, data =  numbani.data, result = [];
-    data.forEach( ( item, idx ) => {
-      result.push( tmpl.call( null, item ) );
-    });
-    return result.join('');
   }
 }
